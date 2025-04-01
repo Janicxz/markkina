10afdb588671be03ced3562c6f4108366ef1e5b0
Janicxz <janizu99@gmail.com>
2025-04-01 18:32:51 +0300

    Create kantayhteys.php

---
+++ kantayhteys.php
@@ -0,0 +1,15 @@
+<?php
+    $servername = "localhost";
+    $username = "markkina";
+    $password = "markkina";
+    $database = "markkina";
+    try {
+    $dbconnect = mysqli_connect($servername, $username, $password, $database);
+    } catch (Exception $e) {
+        die("Yhdistäminen tietokantaan epäonnistui:".$e."<br>");
+    }
+    if (!$dbconnect) {
+        die("Yhdistäminen tietokantaan epäonnistui:".mysqli_connect_error()."<br>");
+    }
+    echo "Yhdistettiin tietokantaan onnistuneesti<br>";
+?>
