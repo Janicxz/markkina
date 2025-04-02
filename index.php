c5890ee0d5ab46a64f3f0322c774e98f6f1041b5
Janicxz <janizu99@gmail.com>
2025-04-02 13:41:27 +0300

    Add index.php functionality

--- index.php
+++ index.php
@@ -1,3 +1,26 @@
 <?php
+session_start();
 
+// DEBUG, TODO, poista kun sivu toimii
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+header("Content-Type: text/html; charset=utf-8");
+
+// Asetetaan aikavyöhyke Suomen aikaan
+date_default_timezone_set("Europe/Helsinki");
+
+echo "<h2>Osto- ja myyntipalsta</h2>";
+
+if (isset($_SESSION['LOGGEDIN']) && $_SESSION['LOGGEDIN'] == 1) {
+    echo "Tervetuloa käyttämään palvelua " . $_SESSION["kayttaja_tunnus"] . "!<br>";
+
+    echo "(<a href='lisaailmoitus.php'>Lisää ilmoitus</a>) - (<a href='tiedot.php'>Muuta tietojasi</a>)
+    - (<a href='uloskirjautuminen.php'>Kirjaudu ulos</a>)";
+}
+else {
+    echo "<a href='kirjautuminen.html'>Kirjaudu sisään</a> tai
+    <a href='rekisterointi.html'>rekisteröidy palveluun</a>.";
+}
 ?>
