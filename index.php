c842352389c982bc626e8a78210fecc92eb0ca09
Janicxz <janizu99@gmail.com>
2025-04-02 21:48:22 +0300

    Add item listing and control UI to index.php

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 session_start();
+include("kantayhteys.php");
 
 // DEBUG, TODO, poista kun sivu toimii
 /*
@@ -23,5 +24,85 @@ if (isset($_SESSION['LOGGEDIN']) && $_SESSION['LOGGEDIN'] == 1) {
 else {
     echo "<a href='kirjautuminen.html'>Kirjaudu sisään</a> tai
     <a href='rekisterointi.html'>rekisteröidy palveluun</a>.";
+}
+
+// Ilmoitusten tuonti
+$query = "SELECT * FROM ilmoitukset INNER JOIN kayttajat ON ilmoitukset.myyja_id = kayttajat.kayttaja_id";
+$result = mysqli_query($dbconnect, $query);
+
+if (!$result) {
+    printf("Error: %s\n", mysqli_error($dbconnect));
+    exit();
+}
+
+$num = mysqli_num_rows($result);
+$i = 0;
+while ($i < $num) {
+    $row = mysqli_fetch_assoc($result);
+    $ilmoitus_id = $row["ilmoitus_id"];
+    $ilmoitus_laji = $row["ilmoitus_laji"];
+
+    // Ilmoituksen lajia ei löytynyt
+    if (false === $ilmoitus_laji) {
+        echo mysqli_error($dbconnect);
+    }
+    if ($ilmoitus_laji == 1) {
+        $ilmoitus_laji = "Myydään";
+    }
+    if ($ilmoitus_laji == 2) {
+        $ilmoitus_laji = "Ostetaan";
+    }
+    $ilmoitus_nimi = $row["ilmoitus_nimi"];
+    $ilmoitus_kuvaus = $row["ilmoitus_kuvaus"];
+    $ilmoitus_paivays = $row["ilmoitus_paivays"];
+    $ilmoitus_oikeapaivays = date("d-m-Y", strtotime($ilmoitus_paivays));
+    $myyja_id = $row["myyja_id"];
+    $myyja_tunnus = $row["kayttaja_tunnus"];
+    $myyja_sahkoposti = $row["kayttaja_sahkoposti"];
+
+
+    $poista_ilmoitus_tr = "";
+    if (isset($_SESSION["kayttaja_id"]) && $_SESSION["kayttaja_id"] == $myyja_id ||
+        isset($_SESSION["kayttaja_taso"]) && $_SESSION["kayttaja_taso"] == "admin") {
+        $poista_ilmoitus_tr = "
+        <tr>
+            <td>
+                <form action='poistailmoitus.php' method='post'>
+                    <input type='submit' value='Poista'>
+                    <input type='hidden' name='poista' value='1'>
+                    <input type='hidden' name='poista_id' value='$ilmoitus_id'>
+                </form>
+                <form action='muokkaailmoitus.php' method='post'>
+                    <input type='submit' value='Muokkaa'>
+                    <input type='hidden' name='muokkaa' value='1'>
+                    <input type='hidden' name='muokkaa_id' value='$ilmoitus_id'>
+                </form>
+            </td>
+        </tr>";
+    }
+
+    echo "
+        <p>
+            <table width='500'>
+                <tr>
+                    <td bgcolor='#AABBCC'>
+                        <b>$ilmoitus_laji: $ilmoitus_nimi</b>
+                    </td>
+                </tr>
+                <tr>
+                    <td>$ilmoitus_kuvaus</td>
+                </tr>
+                <tr>
+                    <td>Ilmoitus jätetty: $ilmoitus_oikeapaivays</td>
+                </tr>
+                <tr>
+                    <td>Myyjä: $myyja_tunnus</td>
+                </tr>
+                $poista_ilmoitus_tr
+            </table>
+        </p>
+    ";
+
+    $i++;
 }
 ?>
