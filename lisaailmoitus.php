3a6ecbb400c5489d962464151deedd3195e38e01
Janicxz <janizu99@gmail.com>
2025-04-02 19:31:24 +0300

    Start work on lisaailmoitus.php

---
+++ lisaailmoitus.php
@@ -0,0 +1,10 @@
+<?php
+session_start();
+include("kantayhteys.php");
+
+ini_set("display_errors", 1);
+ini_set("display_startup_errors", 1);
+error_reporting(E_ALL);
+
+header("Content-Type: text/html; charset=utf-8");
+?>
