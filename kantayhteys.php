db959d850c1ae91fb30845af3a11ca0d04189e1a
Janicxz <janizu99@gmail.com>
2025-04-02 13:14:55 +0300

    Clean up

--- kantayhteys.php
+++ kantayhteys.php
@@ -1,15 +1,15 @@
 <?php
-    $servername = "localhost";
-    $username = "markkina";
-    $password = "markkina";
-    $database = "markkina";
-    try {
+$servername = "localhost";
+$username = "markkina";
+$password = "markkina";
+$database = "markkina";
+try {
     $dbconnect = mysqli_connect($servername, $username, $password, $database);
-    } catch (Exception $e) {
-        die("Yhdistäminen tietokantaan epäonnistui:".$e."<br>");
-    }
-    if (!$dbconnect) {
-        die("Yhdistäminen tietokantaan epäonnistui:".mysqli_connect_error()."<br>");
-    }
-    echo "Yhdistettiin tietokantaan onnistuneesti<br>";
+} catch (Exception $e) {
+    die("Yhdistäminen tietokantaan epäonnistui:" . $e . "<br>");
+}
+if (!$dbconnect) {
+    die("Yhdistäminen tietokantaan epäonnistui:" . mysqli_connect_error() . "<br>");
+}
+echo "Yhdistettiin tietokantaan onnistuneesti<br>";
 ?>
